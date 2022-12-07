8c590f910f5ba515edda0031dd14e99719822c03
chibwesamuel <smchibwe@gmail.com>
2022-12-07 05:49:41 -0800

    Added New Line

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,4 @@
 #File: 0-square_matrix_simple.py
 
 def square_matrix_simple(matrix=[]):
-    return ([list(map(lambda x: x * x, row)) for row in matrix]\n)
+    return ([list(map(lambda x: x * x, row)) for row in matrix] end='\n')
