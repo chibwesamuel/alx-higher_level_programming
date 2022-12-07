1c02cf019e404e335548e256c200825a94de1dee
chibwesamuel <smchibwe@gmail.com>
2022-12-07 05:36:18 -0800

    Added Task 0 & README

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+#Author: Samuel Mukosa Chibwe
+#File: 0-square_matrix_simple.py
+
+def square_matrix_simple(matrix=[]):
+    matrix = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+    ]
+
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix\n)
+    #print(matrix)
