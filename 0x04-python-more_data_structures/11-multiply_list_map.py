6b82d66a8e4513623335c7ea3e05968f143f465c
chibwesamuel <smchibwe@gmail.com>
2022-12-07 23:11:03 -0800

    All tasks done

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author: Samuel Mukosa Chibwe
+# File: 11-multiply_list_map.py
+
+def mutiply_list_map(my_list=[], number=0):
+    return (list(map(lambda x: x*number, my_list)))
