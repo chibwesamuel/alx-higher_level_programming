3e2aeb4b9a6fc6e348e7a499b439bbb112c62ab9
chibwesamuel <smchibwe@gmail.com>
2023-03-24 10:13:24 -0700

    Corrected documentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,12 +3,12 @@
 # Usage: ./0-select_states.py <mysql username> \
 #                             <mysql password> \
 #                             <database name>
-"""python3 -c 'print(__import__("my_module").__doc__)'
-"""
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    """python3 -c 'print(__import__("my_module").__doc__)' """
     db = MySQLdb.connect(username=sys.argv[0], passwd=sys.argv[1], db=sys.argv[2])
     c = db.cursor()
     c.execute("SELECT * FROM states ORDER BY states.id")
