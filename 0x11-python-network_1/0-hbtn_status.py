dd30aff67647b53099063a5144557fccaa64ec7a
chibwesamuel <smchibwe@gmail.com>
2023-03-31 04:16:24 -0700

    Formatted task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 # Fetches a given url
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read()
+        print("body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
